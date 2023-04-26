10aa11bd03b24912cc96e0dbccfe6e7519923059
Jaydeep <jaydeep.dey03@gmail.com>
2023-04-26 19:44:13 +0530

    added build cmd

--- API/start.sh
+++ API/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 python3.8 -m pip install --upgrade pip
-sudo apt-get update && sudo apt-get install python3.10
+apt-get update
+apt-get install python3.10
+apt-get install -y sudo
 pip install -r requirements.txt
 python3 main.py
